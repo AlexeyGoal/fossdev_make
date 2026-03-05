b7ff927f406a5649066e9a8a658808256b6b50d4
sensei38 <shihowdima1337@gmail.com>
2026-03-05 13:16:56 +0800

    added cluster test

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -8,7 +8,7 @@ def test_addition():
     print("assert passed")
 
 def test_addition_with_bug():
-    assert add_with_bug(2,3) == 5
+    assert add_with_bug(2,3) == 6
     assert add_with_bug(2,2) == 4
     print("test bugged passed")
 
@@ -23,8 +23,19 @@ def test_addition_overkill():
             assert add(-i,j) == -i + j
             assert add(i,-j) == i - j
 
+def test_addition_clusters():
+    assert add(7,6) == 13
+    assert add(0,6) == 6
+    assert add(7,0) == 7
+    assert add(10,-11) == -1
+    assert add(-10, -11) == -21
+    assert add(-5,0) == -5
+    assert add(0,-2) == -2
+    print("Test clusters passed")
+
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_duplicate()
+    test_addition_clusters()
     # test_addition_overkill() can try
