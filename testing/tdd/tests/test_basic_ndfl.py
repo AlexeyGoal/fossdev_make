84bc47232c59afadb62b8906fdab1a5b92438a5f
sensei38 <shihowdima1337@gmail.com>
2026-03-10 15:11:26 +0800

    added negative test

--- testing/tdd/tests/test_basic_ndfl.py
+++ testing/tdd/tests/test_basic_ndfl.py
@@ -1,11 +1,11 @@
 
 from ndfl import calculate_ndfl
-
+import pytest
 def test_ndfl_tier_1_basic():
     assert calculate_ndfl(2_000_000) == 260_000
 
 def test_ndfl_tier2_basic():
-    assert calculate_ndfl(4_000_00) == 552_000
+    assert calculate_ndfl(4_000_000) == 552_000
 
 def test_ndfl_tier3_basic():
     assert calculate_ndfl(10_000_000) == 1_602_000
@@ -17,3 +17,6 @@ def test_ndfl_tier4_basic():
 def test_ndfl_tier5_basic():
     assert calculate_ndfl(60_000_000) == 11_602_000
 
+@pytest.mark.xfail
+def test_ndfl_false_negative_income():
+    calculate_ndfl(-1000)
